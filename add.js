e4fe7aa87385bdcbbbbc4681fe1974d2f59ad880
dacphuongotp <dacphuongotp@gmail.com>
2022-03-07 16:33:17 +0700

    delete

--- add.js
+++
@@ -1,4 +0,0 @@
-var demo = function (a, b, c) {
-    console.log(a + b + c);
-};
-demo(1, 2, 3);
--- add.ts
+++
@@ -1,4 +0,0 @@
-const demo = (a:number,b:number,c:number) => {
-    console.log(a+b+c);
-}
-demo(1,2,3);
